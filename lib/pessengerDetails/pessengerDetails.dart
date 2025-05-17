26127958318267ce499ac2ac78e963a91290600e
sajivddin <sajivddin.websolutions@gmail.com>
2025-05-17 18:29:10 +0530

    change

--- lib/pessengerDetails/pessengerDetails.dart
+++ lib/pessengerDetails/pessengerDetails.dart
@@ -45,6 +45,9 @@ class _PessengerDetailsState extends State<PessengerDetails> {
     }
   }
 
+  String selectedValue = "Char Dham";
+  List<String> options = ["Data 1", "Data 2", "Data 3"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -238,6 +241,163 @@ class _PessengerDetailsState extends State<PessengerDetails> {
                     ],
                   ),
                   SizedBox(width: 15.w),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 15.w, right: 15.w, top: 11.h),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 66.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.r),
+                border: Border.all(color: Color(0xFF1E3E62), width: 1.w),
+              ),
+              child: Row(
+                children: [
+                  SizedBox(width: 15.w),
+                  Text(
+                    "Total Number of people",
+                    style: GoogleFonts.inter(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF0B192C),
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    width: 55.w,
+                    height: 40.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.r),
+                      color: Color(0xFFFBECE8),
+                    ),
+                    child: TextField(
+                      keyboardType: TextInputType.numberWithOptions(),
+                      style: TextStyle(height: 0.9),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 18.h),
+                        enabledBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.r),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.r),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20.w),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 13.h),
+          Padding(
+            padding: EdgeInsets.only(left: 15.w, right: 15.w),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.r),
+                border: Border.all(color: Color(0xFF1E3E62), width: 1.w),
+              ),
+              child: ExpansionTile(
+                // leading: Container(
+                //   width: 10.w,
+                //   height: 10.h,
+                //   decoration: BoxDecoration(
+                //     color: Colors.black,
+                //     shape: BoxShape.circle,
+                //   ),
+                // ),
+                title: Text(
+                  "Traveller Details",
+                  style: GoogleFonts.inter(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF0B192C),
+                  ),
+                ),
+                shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.r),
+                  side: BorderSide(color: Color(0xFF1E3E62), width: 1.w),
+                ),
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 20.w, right: 20.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Divider(color: Color(0xFFCFCCD2)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Specify Traveller",
+                              style: GoogleFonts.inter(
+                                fontSize: 14.sp,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xFF0B192C),
+                              ),
+                            ),
+                            PopupMenuButton<String>(
+                              onSelected: (value) {
+                                setState(() {
+                                  setState(() {
+                                    selectedValue = value;
+                                  });
+                                });
+                              },
+                              itemBuilder:
+                                  (context) => [
+                                    PopupMenuItem<String>(
+                                      value: 'Option 1',
+                                      child: Text('Option 1'),
+                                    ),
+                                    PopupMenuItem<String>(
+                                      value: 'Option 2',
+                                      child: Text('Option 2'),
+                                    ),
+                                    PopupMenuItem<String>(
+                                      value: 'Option 3',
+                                      child: Text('Option 3'),
+                                    ),
+                                  ],
+                              child: Container(
+                                width: 149.w,
+                                height: 25.h,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8.r),
+                                  color: Color(0xFFFBECE8),
+                                ),
+                                child: Row(
+                                  children: [
+                                    SizedBox(width: 15.w),
+                                    Text(
+                                      "$selectedValue",
+                                      style: GoogleFonts.inter(
+                                        fontSize: 14.sp,
+                                        fontWeight: FontWeight.w400,
+                                        color: Color(0xFF0B192C),
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Icon(
+                                      Icons.keyboard_arrow_down,
+                                      color: Color(0xFF0B192C),
+                                    ),
+                                    SizedBox(width: 10.w),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
